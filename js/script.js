16035aa41d39b7332b12cffe771d1cf1daeea4ce
muftimuf <tiimuf.personal@gmail.com>
2022-06-18 20:21:57 +0700

    add google analytics

--- js/script.js
+++ js/script.js
@@ -49,5 +49,5 @@ var loader = document.querySelector(".loader");
 window.addEventListener("load", vanish);
 
 function vanish() {
-  loader.classList.add("disppear");
+  loader.classList.add("disappear");
 }
